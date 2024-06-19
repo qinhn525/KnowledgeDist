fc69ab85f1392d9679b011741f7ca9413fc80739
Qin Haonan <qinhaonans@163.com>
2024-06-19 10:02:04 +0800

    完成训练流程和外部测试流程的搭建

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from datasets import Dataset, DatasetDict
 from torch.utils.data import DataLoader
 from config import parsing_finetune
 from loguru import logger
+from tqdm import tqdm
+from sklearn.metrics import f1_score
 import  json
 
 def load_model():
@@ -24,17 +26,17 @@ def load_train_valid():
         "valid": "/home/qhn/Codes/Projects/KnowledgeDist/Data/engineer_test.tsv"
     }
     label2index = json.load(open("/home/qhn/Codes/Projects/KnowledgeDist/Data/label2index.json", "r"))
-    train_data, test_data = {"text": [],"label": []}, {"text": [],"label": []}
+    train_data, test_data = {"text": [],"labels": []}, {"text": [],"labels": []}
     with open(data_paths["train"], "r") as f:
         for line in f.readlines():
             text, label = line.strip().split("\t")
             train_data['text'].append(text)
-            train_data['label'].append(label2index[label])
+            train_data['labels'].append(label2index[label])
     with open(data_paths["valid"], "r") as f:
         for line in f.readlines():
             text, label = line.strip().split("\t")
             test_data['text'].append(text)
-            test_data['label'].append(label2index[label])
+            test_data['labels'].append(label2index[label])
     return DatasetDict(
         {
             "train": Dataset.from_dict(train_data),
@@ -44,12 +46,13 @@ def load_train_valid():
 
 def main(args):
     accelerator = Accelerator()
-    logger.add(
-        args.log_path,
-        encoding="utf-8",
-        format="{level} | {time:YYYY-MM-DD HH:mm:ss} | {file} | {line} | {message}",
-        rotation="500 MB"
-    )
+    if accelerator.is_local_main_process:
+        logger.add(
+            args.log_path,
+            encoding="utf-8",
+            format="{level} | {time:YYYY-MM-DD HH:mm:ss} | {file} | {line} | {message}",
+            rotation="500 MB"
+        )
     model, tokenizer = load_model()
     datasets = load_train_valid()
     train_dataset = datasets['train']
@@ -57,14 +60,14 @@ def main(args):
 
     test_dataset = test_dataset.map(
         lambda e: tokenizer(e['text'], truncation=True, padding='max_length', max_length=args.max_seq_length), batched=True)
-    test_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+    test_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
     test_loader = DataLoader(test_dataset, batch_size=130, shuffle=False, drop_last=False,
-                             num_workers=8)
+                             num_workers=0)
     train_dataset = train_dataset.map(
         lambda e: tokenizer(e['text'], truncation=True, padding='max_length', max_length=args.max_seq_length), batched=True)
-    train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
-    train_loader = DataLoader(train_dataset, batch_size=130, shuffle=True, drop_last=False,
-                              num_workers=8) #consider batch size
+    train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+    train_loader = DataLoader(train_dataset, batch_size=130, shuffle=False, drop_last=False,
+                              num_workers=0)
     optimizer = AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr,
                           weight_decay=args.weight_decay)
     lr_scheduler = get_scheduler(
@@ -75,6 +78,49 @@ def main(args):
     )
     model, optimizer, train_loader, test_loader = accelerator.prepare(model, optimizer, train_loader, test_loader)
     logger.info(f"accelerator.state: {accelerator.state}")
+    
+    progress_bar_train = tqdm(range(len(train_loader)), disable=not accelerator.is_local_main_process)
+    
+    steps = 0
+    trained_data = []
+    for _, batch in enumerate(train_loader):
+        # 正向、反向传播
+        model.train()
+        optimizer.zero_grad()
+        outputs = model(**batch)
+        loss = outputs.loss
+        accelerator.backward(loss)
+        
+        # 更新参数
+        optimizer.step()
+        lr_scheduler.step()
+        progress_bar_train.update(1)
+        steps += 1
+        if accelerator.is_main_process:
+            logger.info(f"loss: {loss}, steps: {steps}")
+        
+        # 测试准确率和F1
+        accelerator.wait_for_everyone()
+        model.eval()
+        label_list = []
+        pred_list = []
+        progress_bar_test = tqdm(range(len(test_loader)), disable=not accelerator.is_local_main_process)
+        for _, valid in enumerate(test_loader):
+            res = model(**valid)
+            pred = res.logits.max(1)[1]
+            
+            predictions = accelerator.gather(pred).cpu().numpy().tolist()
+            references = accelerator.gather(valid['labels']).cpu().numpy().tolist()
+            
+            label_list += references
+            pred_list += predictions
+            progress_bar_test.update(1)
+        accelerator.wait_for_everyone()
+        micro_f1 = f1_score(label_list, pred_list, average='micro')
+        accuracy = sum([float(label_list[i] == pred_list[i]) for i in range(len(label_list))]) * 1.0 / len(pred_list)
+        if accelerator.is_local_main_process:
+            logger.info(f"batch {_}: micro_f1: {micro_f1}, accuracy: {accuracy}")
+        
 
 if __name__ == '__main__':
     args = parsing_finetune()
